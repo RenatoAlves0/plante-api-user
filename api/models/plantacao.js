cc415cb47b4c974629658212fd59029b556debaf
renatoalves0 <renatoalves97.123@gmail.com>
2019-09-08 14:54:04 -0300

    campo COR em plantacao

--- api/models/plantacao.js
+++ api/models/plantacao.js
@@ -4,9 +4,10 @@ const plantacao = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     nome: { type: String, required: true },
     cultura: { type: String, required: true }, //pegar o _id da planta do 'plante-api'
-    localizacao: { type: String, required: false },
+    localizacao: { type: String, required: true },
     cidade: { type: mongoose.Schema.Types.ObjectId, ref: 'Cidade', required: true },
-    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }
+    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+    cor: { type: Number, required: false, default: 0 }
 })
 
 module.exports = mongoose.model('Plantacao', plantacao)
--- api/routes/plantacaos.js
+++ api/routes/plantacaos.js
@@ -10,7 +10,8 @@ router.post('/', (req, res, next) => {
         cultura: req.body.cultura,
         localizacao: req.body.localizacao,
         cidade: req.body.cidade,
-        usuario: req.body.usuario
+        usuario: req.body.usuario,
+        cor: req.body.cor
     })
 
     plantacao.save()
