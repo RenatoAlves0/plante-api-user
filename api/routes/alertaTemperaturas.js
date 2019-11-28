32e5f45b88d2973425d83a3d8c4d5bc13cfa1f1d
renatoalves0 <renatoalves97.123@gmail.com>
2019-11-28 19:16:15 -0300

    deletar alertaTemperatura por data tentativa 4

--- api/routes/alertaTemperaturas.js
+++ api/routes/alertaTemperaturas.js
@@ -55,17 +55,17 @@ router.put('/:alertaTemperaturaId', (req, res, next) => {
 })
 
 router.route('/deletar_por_data/:dataParaDeletar')
-    .delete(function (req, res) {
+    .get(function (req, res) {
+        console.log('req.params.dataParaDeletar')
+        console.log(req.params.dataParaDeletar)
         AlertaTemperatura.find()
             .exec()
             .then(async docs => {
                 let alertas = docs.filter(obj => obj.data.split('T')[0] == req.params.dataParaDeletar)
-                console.log('req.params.dataParaDeletar')
-                console.log(req.params.dataParaDeletar)
                 console.log('alertas')
                 console.log(alertas)
                 alertas.forEach(async alerta => {
-                   await AlertaTemperatura.remove({ _id: alerta._id }).exec()
+                    await AlertaTemperatura.remove({ _id: alerta._id }).exec()
                         .then(result => { res.status(200).json({ message: "Deletado com sucesso!" }) })
                         .catch(err => { res.status(500).json({ error: err }) })
                 })
