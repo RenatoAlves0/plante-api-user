63ae18e7b029d251bed4e0971f04640b1bb2ec4f
renatoalves0 <renatoalves97.123@gmail.com>
2019-12-08 14:36:04 -0300

    keep alive changes

--- api/services/keep_alive.js
+++ api/services/keep_alive.js
@@ -1,15 +1,12 @@
 const axios = require('axios')
 
 sleep = (ms) => {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms)
-    })
+    return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 init = async () => {
     await axios.get('https://plante-api-user.herokuapp.com/keepAlive')
-        .then(data => console.log(data.data))
-    await sleep(600000) //600000 ms -> 10 minutos
+    await sleep(600000) //10 minutos
     init()
 }
 
