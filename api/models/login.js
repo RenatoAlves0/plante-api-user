a663164ab36a02b67ca3e9c45750a307c4699bda
renatoalves0 <renatoalves97.123@gmail.com>
2020-11-24 14:08:46 -0300

    login: admin

--- api/models/login.js
+++ api/models/login.js
@@ -4,7 +4,8 @@ const login = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     login: { type: String, required: true },
     senha: { type: String, required: true },
-    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true }
+    usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+    admin: { type: Boolean, default: false }
 })
 
 module.exports = mongoose.model('Login', login)
