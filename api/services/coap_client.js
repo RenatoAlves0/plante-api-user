dd7033273239eb0e9cf4f402ff19af2a38c95b23
renatoalves0 <renatoalves97.123@gmail.com>
2020-12-07 14:21:57 -0300

    iniciando o CoAP

---
+++ api/services/coap_client.js
@@ -0,0 +1,12 @@
+
+const coap = require('coap')
+const req = coap.request({
+    observe: true,
+    query: 'testeoioioi'
+})
+
+req.on('response', function (res) {
+    res.pipe(process.stdout)
+})
+
+req.end()
